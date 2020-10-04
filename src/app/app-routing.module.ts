ed548a1cb8bb4dc476734988c1eed5677b0cfac4
Wan Akashah <wanakashah14833@gmail.com>
2020-10-04 20:31:34 +0800

    Configured web for SolarIoT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { Page404Component } from './page404/page404.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  {path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'homepage', component: HomepageComponent},
   {path: 'login', component: LoginComponent},
   {path: '**', component: Page404Component},
